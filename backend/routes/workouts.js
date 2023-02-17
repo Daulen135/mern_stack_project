5271b7b85464d0ba4e0376c0dbda2b77300eec9d
daulen <daulenz@gmail.com>
2023-02-17 11:49:17 -0600

    backend API CRUD added

--- backend/routes/workouts.js
+++ backend/routes/workouts.js
@@ -1,51 +1,57 @@
 const express = require('express');
-const Workout = require('../models/workoutModel')
-
+// const Workout = require('../models/workoutModel')
+const {createWorkout, getWorkouts, getWorkout, deleteWorkout, updateWorkout} = require('../controllers/workoutController')
 const router  = express.Router();
 
 
 
 
 ///get all workouts
-router.get('/', (req, res)=>{
-    res.json({mssg: "Get All workouts"})
-})
+router.get('/', getWorkouts)
+// router.get('/', (req, res)=>{
+//     res.json({mssg: "Get All workouts"})
+// })
 
 
 //het a signle workout
-
-router.get('/:id', (req, res)=>{
-    res.json({mssg: "get a single workout"})
-})
+router.get('/:id', getWorkout)
+// router.get('/:id', (req, res)=>{
+//     res.json({mssg: "get a single workout"})
+// })
 
 //post a new workout 
+router.post('/', createWorkout)
 
-router.post('/', async (req, res)=>{
-    const {title, load, reps} = req.body;
-    try{
+// router.post('/', async (req, res)=>{
+//     const {title, load, reps} = req.body;
+//     try{
 
-        const workout =await Workout.create({title, load, reps})
-        res.status(200).json(workout)
+//         const workout =await Workout.create({title, load, reps})
+//         res.status(200).json(workout)
 
-    }catch (error){
+//     }catch (error){
 
-        res.status(400).json({error: error.message})
-    }
+//         res.status(400).json({error: error.message})
+//     }
     //  res.json({mssg: "POST a new workout"})
-})
+// })
 
 
 //delete workout
 
-router.delete('/:id', (req, res)=>{
-     res.json({mssg: "delete a workout"})
-})
+router.delete('/:id', deleteWorkout);
+
+// router.delete('/:id', (req, res)=>{
+//      res.json({mssg: "delete a workout"})
+// })
 
 //update workout 
 
-router.patch('/:id', (req, res)=>{
-     res.json({mssg: "update a workout"})
-})
+router.patch('/:id', updateWorkout)
+
+// router.patch('/:id', (req, res)=>{
+//      res.json({mssg: "update a workout"})
+// })
 
 
 
