078d0996c0f3c1698e1b7065aaa309f5363ce528
daulen <daulenz@gmail.com>
2023-02-24 19:49:59 -0600

    Auth basis set up

--- backend/server.js
+++ backend/server.js
@@ -1,46 +1,36 @@
 require('dotenv').config()
-const mongoose = require('mongoose')
-
 
 const express = require('express')
+const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
 
-//express app
-const app = express();
+// express app
+const app = express()
 
+// middleware
+app.use(express.json())
 
-//midleware
-
-app.use(express.json());//handle body of reuest
-
-
-
-app.use((req, res, next)=>{
-    console.log(req.path, req.method)
-    next();
+app.use((req, res, next) => {
+  console.log(req.path, req.method)
+  next()
 })
 
-//routes
+// routes
 app.use('/api/workouts', workoutRoutes)
+app.use('/api/user', userRoutes)
 
-
-
-
-//connect to db
-
-mongoose
-  .connect(process.env.MONGO_URI)
+// connect to db
+mongoose.connect(process.env.MONGO_URI)
   .then(() => {
-    //listen for request s
-
+    // listen for requests
     app.listen(process.env.PORT, () => {
-      console.log("conneted to DB and listening on port ", process.env.PORT);
-    });
+      console.log('connected to db & listening on port', process.env.PORT)
+    })
   })
   .catch((error) => {
-    console.log(error);
-  });
-
+    console.log(error)
+  })
 
 
 //listen for request s
