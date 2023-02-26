f3d169045a7a12a160647e7b5161a5ab5a39c2c5
daulen <daulenz@gmail.com>
2023-02-26 17:17:10 -0600

    Login and Signup forms added

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,23 +1,32 @@
-import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-//pages and compoennents
-import Home from './pages/Home';
-import Navbar from './components/Navbar';
+// pages & components
+import Home from './pages/Home'
+import Login from './pages/Login'
+import Signup from './pages/Signup'
+import Navbar from './components/Navbar'
+
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
         <Navbar />
-         <div className='pages'>
+        <div className="pages">
           <Routes>
-
-            <Route
-              path='/'
-              element={<Home/>}
+            <Route 
+              path="/"
+              element={<Home />}
+            />
+            <Route 
+              path="/login" 
+              element={<Login />} 
+            />
+            <Route 
+              path="/signup" 
+              element={<Signup />} 
             />
           </Routes>
-
-         </div>
+        </div>
       </BrowserRouter>
     </div>
   );
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,18 +1,22 @@
-import React from 'react'
 import { Link } from 'react-router-dom'
 
-export default function Navbar() {
+const Navbar = () => {
+
   return (
     <header>
-
-   
-        <div className='container'>
-            <Link to="/">
-                <h1>Workout  Buddy</h1>
-            </Link>
-        
-        </div>
-
+      <div className="container">
+        <Link to="/">
+          <h1>Workout Buddy</h1>
+        </Link>
+        <nav>
+          <div>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Signup</Link>
+          </div>
+        </nav>
+      </div>
     </header>
   )
 }
+
+export default Navbar
