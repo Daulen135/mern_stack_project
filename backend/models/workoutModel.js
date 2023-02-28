65a3888c47780f1e14c4cb8f0f54d43a3e8fc85f
daulen <daulenz@gmail.com>
2023-02-28 15:07:12 -0600

    React route protection and Assigning Workouts to Users were added

--- backend/models/workoutModel.js
+++ backend/models/workoutModel.js
@@ -1,27 +1,24 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
-const Schema = mongoose.Schema;
+const Schema = mongoose.Schema
 
 const workoutSchema = new Schema({
   title: {
     type: String,
-    required: true,
+    required: true
   },
-
   reps: {
     type: Number,
-    required: true,
+    required: true
   },
   load: {
-   
-      type: Number,
-      required: true,
-   
+    type: Number,
+    required: true
   },
-  
-}, {timestamps: true}
+  user_id: {
+    type: String,
+    required: true
+  }
+}, { timestamps: true })
 
-);
-
-module.exports = mongoose.model("Workout", workoutSchema);
-
+module.exports = mongoose.model('Workout', workoutSchema)
